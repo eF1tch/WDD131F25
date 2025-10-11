448d3c2f0dc148e1fc360e2e734ab9c383d18f6d
eF1tch <ef1tch@byui.edu>
2025-10-10 18:42:23 -0600

    Added menu and resize functionality, as well as js to allow images to be opened on a model

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,6 +1,42 @@
 const toggleButton = document.querySelector(".menu-toggle");
 const menuLinks = document.querySelector(".menu-links");
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
 
-toggleButton.addEventListener("click", function() {
+function toggleMenu() {
     menuLinks.classList.toggle("active");
+}
+toggleButton.addEventListener("click", toggleMenu)
+
+function handleResize() {
+    const menu = document.querySelector(".menu-links")
+    if (window.innerWidth > 1000){
+        menu.classList.add("active");
+    }
+    else {
+        menu.classList.remove("active")
+    } 
+}
+window.addEventListener("resize", handleResize);
+
+function openModal(e) {
+    const img = e.target.closest('img');
+    if (!img) return;
+        const lowResSrc = img.getAttribute('src');
+        const highResSrc = lowResSrc.replace("-sm","-full");
+        modalImage.src = highResSrc;
+        modal.showModal();
+}
+gallery.addEventListener('click', openModal);
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
 });
