cdd65aa0528f6c512338f3d3eb0e9442d37148ca
eF1tch <ef1tch@byui.edu>
2025-11-15 09:35:41 -0700

    Added favicon with dark mode varient, and added js to make it work

---
+++ recipes/main.js
@@ -0,0 +1,20 @@
+const selectElem = document.querySelector('select');
+const logo = document.getElementById('favicon');
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add("dark");
+  logo.href = "images/recipe-book-dark-mode.png";
+}
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+  const current = selectElem.value;
+  if (current === 'dark') {
+    document.body.classList.add("dark");
+    logo.href = "images/recipe-book-dark-mode.png";
+  } else {
+    document.body.classList.remove("dark");
+    logo.href = "images/recipe-book.png";
+  }
+}
